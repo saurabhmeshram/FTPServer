6bb56096f9a4b5651de8306e365f3e3d44d87eb3
Saurabh Meshram <saurabhmesh17@gmail.com>
2014-09-18 14:03:53 +0530

    Initial Code Directory Structure

---
+++ include/common_head.h
@@ -0,0 +1,7 @@
+#ifndef __COMMON_HEAD_H_
+#define __COMMON_HEAD_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "common_head.h"
+
+int main (void)
+{
+	return 0;
+}
